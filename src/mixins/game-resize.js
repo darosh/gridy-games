95329fc49573b8b5f9303b93822f9d212d2990f1
darosh <ensonador@gmail.com>
2018-03-17 20:26:12 +0100

    resize on drawer

--- src/mixins/game-resize.js
+++ src/mixins/game-resize.js
@@ -1,27 +1,35 @@
+import { Shared } from '../services/shared'
+
 export default {
   data () {
     return {
+      Shared,
       vertical: window.innerWidth < window.innerHeight
     }
+  },
+  watch: {
+    'Shared.drawer': function () { this.onResize() }
   },
   methods: {
     onResize () {
       let bounds = this.game.grid.bounds()
+      const innerWidth = window.innerWidth - ((Shared.drawer && (window.innerWidth >= 1264)) ? 300 : 0)
+      const innerHeight = window.innerHeight
 
       if (this.game.landscape) {
         this.vertical = false
       } else {
         const ratio = (bounds.maxX - bounds.minX) / (bounds.maxY - bounds.minY)
-        this.vertical = ratio > 1 && window.innerWidth < window.innerHeight
+        this.vertical = ratio > 1 && innerWidth < innerHeight
       }
 
       if (this.vertical) {
-        this.frame = [window.innerHeight - 64 - 16, window.innerWidth - 32]
+        this.frame = [innerHeight - 64 - 16, innerWidth - 32]
       } else {
-        if (window.innerHeight < 800) {
-          this.frame = [window.innerWidth - 32, window.innerHeight - 64 - 16]
+        if (innerHeight < 800) {
+          this.frame = [innerWidth - 32, innerHeight - 64 - 16]
         } else {
-          this.frame = [window.innerWidth - 32, window.innerHeight - 64 * 2]
+          this.frame = [innerWidth - 32, innerHeight - 64 * 2]
         }
       }
 
