f7dfdca0fd9814c02345cdfd905a3d82f58c2e18
darosh <ensonador@gmail.com>
2018-03-17 18:41:02 +0100

    landscape fix

--- lib/src/games/TriTicTacToeGame.ts
+++ lib/src/games/TriTicTacToeGame.ts
@@ -7,6 +7,8 @@ export class TriTicTacToeGame extends ConnectGameBase {
   public static original = "TicTacToeGame";
   public static wip = true;
 
+  public landscape = true;
+
   constructor() {
     super(new TriangularGrid(1, false, Shape.Triangular, 3, 3) as any, 3);
   }
