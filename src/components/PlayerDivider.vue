65d254811d33ef42b8720495337a60871be41ac4
darosh <ensonador@gmail.com>
2018-03-18 08:39:10 +0100

    lint

--- src/components/PlayerDivider.vue
+++ src/components/PlayerDivider.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    :class="'divider-' + (this.value ? divider : '-1')"
+    :class="'divider-' + (value ? divider : '-1')"
     width="24"
     height="24"
     xmlns="http://www.w3.org/2000/svg"
