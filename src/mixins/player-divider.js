4d7f1cbe5bfe9b4a0ada0b316152262f9477e773
darosh <ensonador@gmail.com>
2018-03-25 11:09:02 +0200

    refactor

--- src/mixins/player-divider.js
+++ src/mixins/player-divider.js
@@ -1,19 +1,23 @@
-import {Shared} from '../services/shared'
+import { Shared } from '../services/shared'
 
 export default {
   computed: {
     playerDivider () {
       if (Shared.game.moves.length && Shared.game.score) {
-        if (Shared.game.score[1] > Shared.game.score[2]) {
-          return 1
-        } else if (Shared.game.score[1] < Shared.game.score[2]) {
-          return 2
-        } else {
-          return 0
-        }
+        return scoreStatus()
       } else {
         return -1
       }
     }
+  }
+}
+
+function scoreStatus () {
+  if (Shared.game.score[1] > Shared.game.score[2]) {
+    return 1
+  } else if (Shared.game.score[1] < Shared.game.score[2]) {
+    return 2
+  } else {
+    return 0
   }
 }
