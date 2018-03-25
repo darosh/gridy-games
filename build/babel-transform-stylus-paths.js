b4a4cf50f6f712ae72ab0ff7d353bea95366afc7
darosh <ensonador@gmail.com>
2018-03-25 11:19:43 +0200

    ref

--- build/babel-transform-stylus-paths.js
+++ build/babel-transform-stylus-paths.js
@@ -14,6 +14,6 @@ function listener (path) {
 }
 
 function transform (matches, path) {
-  const m = matches[0].startsWith('./') ? matches[0].substr(2) : matches[0];
-  path.node.value = path.node.value.replace(matches[0], `${m}../src/`);
+  const m = matches[0].startsWith('./') ? matches[0].substr(2) : matches[0]
+  path.node.value = path.node.value.replace(matches[0], `${m}../src/`)
 }
