e2c9e261cdb0eb004b02cb9c8f738d1b6d9276ce
darosh <ensonador@gmail.com>
2018-03-25 13:13:41 +0200

    ref

--- src/mixins/board-size.js
+++ src/mixins/board-size.js
@@ -28,13 +28,12 @@ export default {
       }
 
       this.game.grid.scale = 1
-      let bounds = this.game.grid.bounds()
 
+      let bounds = this.game.grid.bounds()
       let scaled = scaleToFit(bounds.maxX - bounds.minX, bounds.maxY - bounds.minY, this.frame[0], this.frame[1])
 
       if (!this.game.grid.irregular && this.round) {
         this.game.grid.scale = Math.floor(scaled / 2) * 2
-        // this.game.grid.scale -= this.game.grid.scale % 2
       } else {
         this.game.grid.scale = scaled
       }
@@ -49,17 +48,7 @@ export default {
         this.size = [bounds.maxX - bounds.minX + this.margin, this.frame[1] + this.margin]
       }
 
-      const w = Math.floor(bounds.maxX - bounds.minX) % 2 ? 0 : 0.5
-      const h = Math.floor(bounds.maxY - bounds.minY) % 2 ? 0 : 0.5
-
-      this.translate = [
-        ((this.size[0] - bounds.minX - bounds.maxX) / 2 + w).toFixed(3),
-        ((this.size[1] - bounds.minY - bounds.maxY) / 2 + h).toFixed(3)
-      ]
-
-      // if (this.frame[0] < this.frame[1] && this.size[0] > this.size[1]) {
-      //   this.size.reverse()
-      // }
+      this.translate = getTranslate(bounds, this.size)
     },
     path (tile) {
       return this.game.grid
@@ -104,4 +93,14 @@ export default {
       immediate: true
     }
   }
+}
+
+function getTranslate (bounds, size) {
+  const w = Math.floor(bounds.maxX - bounds.minX) % 2 ? 0 : 0.5
+  const h = Math.floor(bounds.maxY - bounds.minY) % 2 ? 0 : 0.5
+
+  return [
+    ((size[0] - bounds.minX - bounds.maxX) / 2 + w).toFixed(3),
+    ((size[1] - bounds.minY - bounds.maxY) / 2 + h).toFixed(3)
+  ]
 }
