c3037e0852318778b68fea84f3ae596d8bf9ffbd
darosh <ensonador@gmail.com>
2018-03-25 14:14:43 +0200

    ref

--- lib/src/games/evaluate/connect.ts
+++ lib/src/games/evaluate/connect.ts
@@ -187,27 +187,29 @@ export function winning(move: any, player: number, min: number): AnyTile[] | und
     const result = [move];
     done[-n[0]] = true;
 
-    let o: any = n[1];
-    let v = 1;
-
-    while (o && (o.data === player)) {
-      result.push(o);
-      v++;
-      o = o.links.get(n[0]);
-      o = (o !== move) ? o : false;
-    }
-
-    o = (move as any).links.get(-n[0]);
-
-    while (o && (o.data === player)) {
-      result.unshift(o);
-      v++;
-      o = o.links.get(-n[0]);
-      o = (o !== move) ? o : false;
-    }
+    const v = getWinning(n, player, result, move);
 
     if (v >= min) {
       return result;
     }
   }
+}
+
+function getWinning(n: any, player: number, result: any[], move: any) {
+  let o: any = n[1];
+  let v = 1;
+  while (o && (o.data === player)) {
+    result.push(o);
+    v++;
+    o = o.links.get(n[0]);
+    o = (o !== move) ? o : false;
+  }
+  o = (move as any).links.get(-n[0]);
+  while (o && (o.data === player)) {
+    result.unshift(o);
+    v++;
+    o = o.links.get(-n[0]);
+    o = (o !== move) ? o : false;
+  }
+  return v;
 }
