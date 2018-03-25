ca97b9d31439653254de888e50d7d74daea0d28c
darosh <ensonador@gmail.com>
2018-03-25 15:06:03 +0200

    ref

--- lib/src/utils/actions.ts
+++ lib/src/utils/actions.ts
@@ -1,4 +1,4 @@
-import { ICompoundStep, IGameTile, IGridGame, Move } from "../IGridGame";
+import { IAction, ICompoundStep, IGameTile, IGridGame, Move } from "../IGridGame";
 
 function getMovePlace(move: Move, cursor: number = 0): IGameTile | undefined {
   if (Array.isArray(move)) {
@@ -38,22 +38,7 @@ export function initActions(game: IGridGame, moves: Move[], cursor: number = 0) 
 
 export function selectAction(game: IGridGame, tile: IGameTile) {
   if (tile.actions) {
-    if (tile.actions.length === 1) {
-      const action = tile.actions[0];
-      const place = getMovePlace(action.move, action.cursor);
-
-      if (!place) {
-        return action.move;
-      }
-
-      undoAction(game);
-      initActions(game, [action.move], action.cursor);
-    } else {
-      const moves = tile.actions.map((a) => a.move);
-      const action = tile.actions[0];
-      undoAction(game);
-      initActions(game, moves, action.cursor);
-    }
+    return getAction(game, tile.actions);
   } else {
     return false;
   }
@@ -70,5 +55,24 @@ export function undoAction(game: IGridGame) {
         i.actions = undefined;
       }
     }
+  }
+}
+
+function getAction(game: IGridGame, actions: IAction[]) {
+  if (actions.length === 1) {
+    const action = actions[0];
+    const place = getMovePlace(action.move, action.cursor);
+
+    if (!place) {
+      return action.move;
+    }
+
+    undoAction(game);
+    initActions(game, [action.move], action.cursor);
+  } else {
+    const moves = actions.map((a) => a.move);
+    const action = actions[0];
+    undoAction(game);
+    initActions(game, moves, action.cursor);
   }
 }
