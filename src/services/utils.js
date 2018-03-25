2475b7b00230c9b60295b22128f0600244cf5fb0
darosh <ensonador@gmail.com>
2018-03-25 11:06:13 +0200

    refactor

--- src/services/utils.js
+++ src/services/utils.js
@@ -6,12 +6,16 @@ export function scaleToFit (iw, ih, fw, fh) {
 
 export function gameData (game, data, original = true) {
   if ((game[data] === undefined) && original && game.original) {
-    if (Games[game.original][data] === undefined) {
-      return gameData(Games[game.original], data, original)
-    }
-
-    return Games[game.original][data]
+    return gameOriginalData(game, data, original)
   } else {
     return game[data]
   }
+}
+
+function gameOriginalData (game, data, original) {
+  if (Games[game.original][data] === undefined) {
+    return gameData(Games[game.original], data, original)
+  }
+
+  return Games[game.original][data]
 }
