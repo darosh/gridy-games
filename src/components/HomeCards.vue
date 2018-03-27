67b32582e32b2e4d0be50fc69a1ada5f9fad4449
darosh <ensonador@gmail.com>
2018-03-27 22:41:27 +0200

    layout update

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -1,29 +1,71 @@
 <template>
-  <v-container grid-list-lg fluid>
-    <v-layout row wrap>
-      <v-flex v-for="game in Shared.items" :key="game.id" xs12 sm6 md3 lg2 pt-2 pb-2>
-        <v-card :to="{name: 'game', params: {id: game.id}}" height="100%" class="gpu">
+  <v-container
+    grid-list-lg
+    fluid>
+    <v-layout
+      row
+      wrap>
+      <v-flex
+        v-for="game in Shared.items"
+        :key="game.id"
+        xs12
+        sm6
+        md3
+        lg2
+        pt-2
+        pb-2>
+        <v-card
+          :to="{name: 'game', params: {id: game.id}}"
+          height="100%"
+          class="gpu">
           <v-card-title>
             <div>
-              <div :class="{'red--text': game.wip}" :style="{'text-decoration': game.wip ? 'line-through' : ''}" class="headline">{{ game.title | titled }}</div>
+              <div
+                :class="{'red--text': game.wip}"
+                :style="{'text-decoration': game.wip ? 'line-through' : ''}"
+                class="headline">{{ game.title | titled }}</div>
               <span class="grey--text">{{ game.type }}</span>
             </div>
           </v-card-title>
           <div style="min-height: 116px; position: relative">
-            <div v-observe-visibility="!show ? null : (isVisible, entry) => visibilityChanged(isVisible, entry, game.id)" v-show="show" class="text-xs-center" style="min-height: 116px">
-              <g-board v-if="initialized[game.id]" v-once v-model="rendered[game.id]" :game="game.instance" :frame="[164,96]" :margin="4" fixed class="preview d-inline-block" />
+            <div
+              v-observe-visibility="!show ? null : (isVisible, entry) => visibilityChanged(isVisible, entry, game.id)"
+              v-show="show"
+              class="text-xs-center"
+              style="min-height: 116px">
+              <g-board
+                v-if="initialized[game.id]"
+                v-once
+                v-model="rendered[game.id]"
+                :game="game.instance"
+                :frame="[164,96]"
+                :margin="4"
+                fixed
+                class="preview d-inline-block" />
             </div>
-            <div v-show="!show || !rendered[game.id]" class="text-xs-center" style="position: absolute; top: 0; left:0; right: 0">
-              <g-icon :game="game.instance" :type="game.grid" :size="iconSize" :box="iconBox" class="preview preview-md d-inline-block" />
+            <div
+              v-show="!show || !rendered[game.id]"
+              class="text-xs-center"
+              style="position: absolute; top: 0; left:0; right: 0">
+              <g-icon
+                :game="game.instance"
+                :type="game.grid"
+                :size="iconSize"
+                :box="iconBox"
+                class="preview preview-md d-inline-block" />
             </div>
           </div>
-          <v-card-text class="pt-0">
-            <v-layout row fill-height align-content-end align-end>
-              <v-flex v-if="game.location" class="grey--text d-flex body-1">{{ game.location }}</v-flex>
-              <v-flex v-if="game.created" class="body-2 text-xs-right">{{ game.created | era }}
-              </v-flex>
-            </v-layout>
-          </v-card-text>
+          <v-layout
+            row
+            class="px-3 pb-2">
+            <v-flex
+              v-if="game.location"
+              class="grey--text d-flex body-1 mb-1">{{ game.location }}</v-flex>
+            <v-flex
+              v-if="game.created"
+              class="body-2 text-xs-right mb-1">{{ game.created | era }}
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
     </v-layout>
@@ -31,27 +73,27 @@
 </template>
 
 <script>
-import { Shared } from "../services/shared";
-import Vue from "vue";
+import { Shared } from '../services/shared'
+import Vue from 'vue'
 
 // const a = [0, 24]
-const b = [2, 19];
-const s = 101;
-const m = 2.5;
-const p = b[1] / (s - m * 2) / 2;
+const b = [2, 19]
+const s = 101
+const m = 2.5
+const p = b[1] / (s - m * 2) / 2
 
 const iconBox = `${b[0] - p * m * 2} ${b[0] - p * m * 2} ${b[1] +
-  p * m * 4} ${b[1] + p * m * 4}`;
+  p * m * 4} ${b[1] + p * m * 4}`
 
-let timeout;
+let timeout
 
 export default {
-  name: "HomeCards",
+  name: 'HomeCards',
   components: {
-    GBoard: () => import("./Board"),
-    GIcon: () => import("./Icon")
+    GBoard: () => import('./Board'),
+    GIcon: () => import('./Icon')
   },
-  data() {
+  data () {
     return {
       iconSize: s,
       Shared,
@@ -59,44 +101,44 @@ export default {
       initialized: {},
       rendered: {},
       show: false
-    };
+    }
   },
   watch: {
-    "Shared.items": {
+    'Shared.items': {
       immediate: true,
-      handler(items) {
+      handler (items) {
         items.forEach(game => {
           if (this.initialized[game.id] === undefined) {
-            Vue.set(this.initialized, game.id, false);
+            Vue.set(this.initialized, game.id, false)
           }
-        });
+        })
       }
     }
   },
-  activated() {
-    this.cancelTimeout();
+  activated () {
+    this.cancelTimeout()
     timeout = setTimeout(() => {
-      this.show = true;
-    }, 600);
+      this.show = true
+    }, 600)
   },
-  deactivated() {
-    this.cancelTimeout();
+  deactivated () {
+    this.cancelTimeout()
     timeout = setTimeout(() => {
-      this.show = false;
-    }, 0);
+      this.show = false
+    }, 0)
   },
   methods: {
-    cancelTimeout() {
+    cancelTimeout () {
       if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
+        clearTimeout(timeout)
+        timeout = null
       }
     },
-    visibilityChanged(isVisible, entry, id) {
+    visibilityChanged (isVisible, entry, id) {
       if (this.show && isVisible && !this.initialized[id]) {
-        Vue.set(this.initialized, id, isVisible);
+        Vue.set(this.initialized, id, isVisible)
       }
     }
   }
-};
+}
 </script>
