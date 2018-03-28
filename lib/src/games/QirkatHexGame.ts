3256d567b5961f4207a5aec291c079624560377f
darosh <ensonador@gmail.com>
2018-03-28 22:46:14 +0200

    fix

--- lib/src/games/QirkatHexGame.ts
+++ lib/src/games/QirkatHexGame.ts
@@ -15,7 +15,7 @@ export class QirkatHexGame extends QirkatGameBase {
   constructor() {
     const grid = new HexagonalGrid(1, true, Shape.Rhombus, SIZE, SIZE);
     normalize(grid);
-    super(<any>grid, SIZE * SIZE * 3);
+    super(<any>grid, SIZE * SIZE * SIZE * 3);
     this.hull = true;
   }
 }
