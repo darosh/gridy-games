837ba4bf20515f223ab022c50987da8948c1cde5
darosh <ensonador@gmail.com>
2018-03-29 12:56:41 +0200

    game tests

--- lib/tests/games/all.test.ts
+++ lib/tests/games/all.test.ts
@@ -1,7 +1,23 @@
-import { Info } from '../../src';
+import { Games, Info, update } from '../../src';
 
 describe('Game infos', () => {
   it('should load', () => {
     expect(Info.games.length).toBeGreaterThan(40);
+  });
+});
+
+Object.keys(Games).forEach((key) => {
+  describe(`Game ${key}`, () => {
+    let game: any;
+
+    it('should create instance', () => {
+      game = new (<any>Games)[key]();
+      expect(game).toBeDefined();
+    });
+
+    it('should load sample moves', () => {
+      update(game, game.constructor.sample);
+      expect(game.moves.length).toBeGreaterThan(0);
+    });
   });
 });
