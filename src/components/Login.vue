f257145ba29db81613e568d8a4943efc3717981c
darosh <ensonador@gmail.com>
2018-04-08 08:46:26 +0200

    login buttons

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="text-xs-center pa-2">
-    <div v-if="!loading">
-      <v-btn class="primary" @click="signInGoogle()">Sign in with Google</v-btn>
-      <v-btn class="primary" @click="signInGitHub()">Sign in as GitHub</v-btn>
-      <v-btn class="primary" @click="signInAnonym()">Sign in as guest</v-btn>
+    <div v-if="!loading" class="login-buttons">
+      <div><v-btn @click="signInGoogle()"><v-icon class="mr-3">google</v-icon> Google login</v-btn></div>
+      <div><v-btn @click="signInGitHub()"><v-icon class="mr-3">github_circle</v-icon> GitHub login</v-btn></div>
+      <div><v-btn @click="signInAnonym()"><v-icon class="mr-3">person</v-icon> Guest login</v-btn></div>
     </div>
     <v-progress-circular v-else
         color="light-blue"
@@ -54,5 +54,10 @@ export default {
 </script>
 
 <style scoped>
-
+.login-buttons >>> .btn__content {
+  justify-content: left;
+}
+.login-buttons > * > * {
+  min-width: 170px;
+}
 </style>
